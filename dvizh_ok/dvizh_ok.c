6b0e220a6742ecb3d5512ae4d899ce463da23108
vlad <vlad-matveev2004@mail.ru>
2023-04-26 20:03:06 +0300

    added test dvizh_ok

---
+++ dvizh_ok/dvizh_ok.c
@@ -0,0 +1,258 @@
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+    #include <stdio.h>
+#endif
+
+const unsigned long long tick = 100; // tick duration in milliseconds
+
+unsigned int circles_size = 4;
+unsigned int polygons_size = 4;
+struct timespec time_prev;
+
+typedef struct _Circle {
+    unsigned int id;
+    double r;
+    double m;
+    double x;
+    double y;
+    double vx;
+    double vy;
+} Circle;
+
+typedef struct _Polygon {
+    unsigned int id;
+} Polygon;
+
+
+Circle** circles;
+Polygon** polygons;
+unsigned int circles_count = 0;
+unsigned int polygons_count = 0;
+
+int init()
+{
+    circles = calloc(circles_size, sizeof(Circle*));
+    polygons = calloc(polygons_size, sizeof(Polygon*));
+    return 0;
+}
+
+// Returns id of new circle
+// Rewrite to optimize memory: scan for null_ptrs
+int add_circle(double r, double m, double x, double y, double vx, double vy)
+{
+    #ifdef DEBUG
+        printf("---\nr: %lf\nm: %lf\nx: %lf\ny: %lf\nvx: %lf\nvy: %lf\n---\n", r, m, x, y, vx, vy);
+    #endif
+    Circle* circle = calloc(1, sizeof(Circle));
+    *circle = (Circle){circles_count, r, m, x, y, vx, vy};
+
+    if (circles_size == circles_count + 1)
+    {
+        circles_size *= 2;
+        circles = realloc(circles, sizeof(Circle*) * circles_size);
+    }
+
+    circles[circles_count++] = circle;
+    return circles_count;
+}
+
+int del_circle(int id)
+{
+    free(circles[id]);
+    circles[id] = 0;
+}
+
+void step(double delta_max)
+{
+    if (delta_max <= 0)
+        return;
+
+    double dt_min = delta_max;
+    int i_min = -1, j_min = -1;
+
+    printf("Finding \"nearest\" circles\n");
+
+    for (int i = 0; i < circles_count; i++)
+    {
+        if (circles[i] == NULL)
+            continue;
+        for (int j = i + 1; j < circles_count; j++)
+        {
+            if (circles[j] == NULL)
+                continue;
+
+            double r = circles[i]->r + circles[j]->r;
+
+            double delta_x = circles[i]->x - circles[j]->x;
+            double delta_y = circles[i]->y - circles[j]->y;
+            double delta_vx = circles[i]->vx - circles[j]->vx;
+            double delta_vy = circles[i]->vy - circles[j]->vy;
+
+            double D = (delta_vx * delta_x + delta_vy * delta_y) * (delta_vx * delta_x + delta_vy * delta_y) - (delta_vx * delta_vx + delta_vy * delta_vy) * (delta_x * delta_x + delta_y * delta_y - r * r);
+            
+            if (D < 0)
+                continue;
+
+            double dt = -(delta_vx * delta_x + delta_vy * delta_y + sqrt(D)) / (delta_vx * delta_vx + delta_vy * delta_vy);
+
+            printf("dt: %lf\n", dt);
+
+            if (dt < 0)
+                continue;
+
+            if (dt_min > dt)
+            {
+                dt_min = dt;
+                i_min = i;
+                j_min = j;
+            }
+        }
+    }
+
+    printf("circles %d, %d finded, moving circles\n", i_min, j_min);
+
+    for (int i = 0; i < circles_count; i++)
+    {
+        if (circles[i] == NULL)
+            continue;
+        circles[i]->x += circles[i]->vx * dt_min;
+        circles[i]->y += circles[i]->vy * dt_min;
+    }
+
+    if (i_min == -1)
+        return;
+
+    //double vx_cm = (circles[i_min]->m * circles[i_min]->vx + circles[j_min]->m * circles[j_min]->vx) / (circles[i_min]->m + circles[j_min]->m);
+    //double vy_cm = (circles[i_min]->m * circles[i_min]->vy + circles[j_min]->m * circles[j_min]->vy) / (circles[i_min]->m + circles[j_min]->m);
+    //double vx1_cm = circles[i_min]->vx - vx_cm;
+    //double vy1_cm = circles[i_min]->vy - vy_cm;
+    //double vx2_cm = circles[j_min]->vx - vx_cm;
+    //double vy2_cm = circles[j_min]->vy - vy_cm;
+
+    //double phi = atan2(circles[j_min]->y - circles[i_min]->y, circles[j_min]->x - circles[i_min]->x);
+
+    printf("Counting impulses\n");
+
+    printf("Step 1\n");
+    fflush(stdin);
+
+    double r = circles[i_min]->r + circles[j_min]->r;
+    double sin_phi = (circles[j_min]->y - circles[i_min]->y) / r;
+    double cos_phi = (circles[j_min]->x - circles[i_min]->x) / r;
+
+    double vn1 = circles[i_min]->vx * cos_phi + circles[i_min]->vy * sin_phi;
+    double vn2 = circles[j_min]->vx * cos_phi + circles[j_min]->vy * sin_phi;
+
+    double vt1 = circles[i_min]->vy * cos_phi - circles[i_min]->vx * sin_phi;
+    double vt2 = circles[j_min]->vy * cos_phi - circles[j_min]->vx * sin_phi;
+
+    double m1 = circles[i_min]->m;
+    double m2 = circles[j_min]->m;
+    
+    printf("Step 2.1\n");
+    fflush(stdin);
+
+    double a, b, c; // ax^2 + 2bx + c = 0
+    a = m1 * (m1 + m2);
+    b = m1 * (m1 * vn1 + m2 * vn2);
+    c = vn1 * vn1 * m1 * (m1 - m2) + 2 * vn1 * vn2 * m1 * m2;
+
+    printf("Step 2.2\n");
+    fflush(stdin);
+
+    double vn1_new = (-b - sqrt(b * b - a * c)) / a;
+    
+    printf("Step 3.1\n");
+    fflush(stdin);
+
+    a, b, c; // ax^2 + 2bx + c = 0
+    a = m2 * (m1 + m2);
+    b = m2 * (m2 * vn2 + m1 * vn1);
+    c = vn2 * vn2 * m2 * (m2 - m1) + 2 * vn1 * vn2 * m1 * m2;
+
+    printf("Step 3.2\n");
+    fflush(stdin);
+
+    double vn2_new = (-b + sqrt(b * b - a * c)) / a;
+    
+    printf("Step 4\n");
+    fflush(stdin);
+
+    circles[i_min]->vx = vn1_new * cos_phi - vt1 * sin_phi;
+    circles[i_min]->vy = vn1_new * sin_phi + vt1 * cos_phi;
+    circles[j_min]->vx = vn2_new * cos_phi - vt2 * sin_phi;
+    circles[j_min]->vy = vn2_new * sin_phi + vt2 * cos_phi;
+
+    step(delta_max - dt_min);
+}
+
+void free_all()
+{
+    for (int i = 0; i < circles_count; i++)
+    {
+        free(circles[i]);
+    }
+    free(circles);
+}
+
+#ifdef DEBUG
+void debug_print()
+{
+    for (int i = 0; i < circles_count; i++)
+    {
+        if (circles[i] == NULL)
+            continue;
+        printf("---\nr: %lf\nm: %lf\nx: %lf\ny: %lf\nvx: %lf\nvy: %lf\n---\n", circles[i]->r, circles[i]->m, circles[i]->x, circles[i]->y, circles[i]->vx, circles[i]->vy);
+    }
+}
+
+/*
+double r;
+double m;
+double x;
+double y;
+double vx;
+double vy;
+*/
+
+int is_null(int i)
+{
+    return circles[i] == NULL;
+}
+
+double get_r(int i)
+{
+    return circles[i]->r;
+}
+
+double get_m(int i)
+{
+    return circles[i]->m;
+}
+
+double get_x(int i)
+{
+    return circles[i]->x;
+}
+
+double get_y(int i)
+{
+    return circles[i]->y;
+}
+
+double get_vx(int i)
+{
+    return circles[i]->vx;
+}
+
+double get_vy(int i)
+{
+    return circles[i]->vy;
+}
+
+#endif
---
+++ dvizh_ok/physic_test.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+import ctypes
+from random import randint
+
+dvizh_ok = ctypes.cdll.LoadLibrary("./dvizh_ok.so")
+
+#dvizh_ok.add_circle(double r, double m, double x, double y, double vx, double vy)
+dvizh_ok.init()
+
+dvizh_ok.add_circle(ctypes.c_double(20), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(20), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(20), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(20), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(50), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(50), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(50), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+dvizh_ok.add_circle(ctypes.c_double(50), ctypes.c_double(1), ctypes.c_double(500 + randint(-200, 200)), ctypes.c_double(250 + randint(-100, 100)), ctypes.c_double(randint(-25, 25)), ctypes.c_double(randint(-25, 25)))
+
+###dvizh_ok.add_circle(ctypes.c_double(30), ctypes.c_double(1), ctypes.c_double(100), ctypes.c_double(200), ctypes.c_double(+25), ctypes.c_double(0))
+###dvizh_ok.add_circle(ctypes.c_double(30), ctypes.c_double(1), ctypes.c_double(200), ctypes.c_double(220), ctypes.c_double(-25), ctypes.c_double(0))
+
+##dvizh_ok.add_circle(ctypes.c_double(10), ctypes.c_double(1), ctypes.c_double(100), ctypes.c_double(100), ctypes.c_double(randint(-2, 2)), ctypes.c_double(randint(-2, 2)))
+#
+#print("ABOBUS")
+#
+##print(dvizh_ok.circles)
+#dvizh_ok.debug_print()
+#
+#print("ABOBUS1")
+#dvizh_ok.del_circle(1)
+#dvizh_ok.del_circle(3)
+#
+#print("ABOBUS2")
+#dvizh_ok.debug_print()
+
+
+print(ctypes.c_int.in_dll(dvizh_ok, "circles_count").value)
+
+#double r;
+#double m;
+#double x;
+#double y;
+#double vx;
+#double vy;
+
+dvizh_ok.is_null.restype = ctypes.c_int
+dvizh_ok.get_r.restype = ctypes.c_double
+dvizh_ok.get_m.restype = ctypes.c_double
+dvizh_ok.get_x.restype = ctypes.c_double
+dvizh_ok.get_y.restype = ctypes.c_double
+dvizh_ok.get_vx.restype = ctypes.c_double
+dvizh_ok.get_vy.restype = ctypes.c_double
+
+for i in range(ctypes.c_int.in_dll(dvizh_ok, "circles_count").value):
+    if dvizh_ok.is_null(ctypes.c_int(i)) == 1:
+        continue
+    
+    print(f"r:  {ctypes.c_double(dvizh_ok.get_r (ctypes.c_int(i))).value}")
+    print(f"m:  {ctypes.c_double(dvizh_ok.get_m (ctypes.c_int(i))).value}")
+    print(f"x:  {ctypes.c_double(dvizh_ok.get_x (ctypes.c_int(i))).value}")
+    print(f"y:  {ctypes.c_double(dvizh_ok.get_y (ctypes.c_int(i))).value}")
+    print(f"vx: {ctypes.c_double(dvizh_ok.get_vx(ctypes.c_int(i))).value}")
+    print(f"vy: {ctypes.c_double(dvizh_ok.get_vy(ctypes.c_int(i))).value}")
+    print("ABOBA")
+
+##### GUI
+
+import pygame
+
+##########################################################################
+WIDTH = 1000  # ширина игрового окна
+HEIGHT = 500 # высота игрового окна
+FPS = 60 # частота кадров в секунду
+TPS = 10 
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+COLORS = [RED, GREEN, BLUE]
+
+##########################################################################
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+###########################################################################
+
+running = True
+frame_counter = 0
+while running:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+
+
+    screen.fill(WHITE)
+
+    for i in range(ctypes.c_int.in_dll(dvizh_ok, "circles_count").value):
+        if dvizh_ok.is_null(ctypes.c_int(i)) == 1:
+            continue
+    
+        #print(f"r:  {ctypes.c_double(dvizh_ok.get_r (ctypes.c_int(i))).value}")
+        #print(f"m:  {ctypes.c_double(dvizh_ok.get_m (ctypes.c_int(i))).value}")
+        #print(f"x:  {ctypes.c_double(dvizh_ok.get_x (ctypes.c_int(i))).value}")
+        #print(f"y:  {ctypes.c_double(dvizh_ok.get_y (ctypes.c_int(i))).value}")
+        print(f"vx: {ctypes.c_double(dvizh_ok.get_vx(ctypes.c_int(i))).value}")
+        print(f"vy: {ctypes.c_double(dvizh_ok.get_vy(ctypes.c_int(i))).value}")
+
+        #x = ctypes.c_double(dvizh_ok.get_x(ctypes.c_int(i))).value + ctypes.c_double(dvizh_ok.get_vx(ctypes.c_int(i))).value * (frame_counter % (FPS / TPS)) / FPS
+        #y = ctypes.c_double(dvizh_ok.get_y(ctypes.c_int(i))).value + ctypes.c_double(dvizh_ok.get_vy(ctypes.c_int(i))).value * (frame_counter % (FPS / TPS)) / FPS
+        x = ctypes.c_double(dvizh_ok.get_x(ctypes.c_int(i))).value
+        y = ctypes.c_double(dvizh_ok.get_y(ctypes.c_int(i))).value
+        r = ctypes.c_double(dvizh_ok.get_r(ctypes.c_int(i))).value
+
+        pygame.draw.circle(screen, COLORS[i % len(COLORS)], [x, y], r, 5)
+
+    pygame.display.flip()
+
+    #if frame_counter % (FPS // TPS) == 0:
+    #    dvizh_ok.step(ctypes.c_double(1/TPS))
+    #frame_counter += 1
+    dvizh_ok.step(ctypes.c_double(1/FPS))
+
+pygame.quit()
