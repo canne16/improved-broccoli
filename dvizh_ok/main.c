12b117c9b1f18dd59983509fca35708c8fc59dab
vlad <vlad-matveev2004@mail.ru>
2023-05-21 18:50:56 +0300

    added min/max commands

--- dvizh_ok/main.c
+++ dvizh_ok/main.c
@@ -5,6 +5,9 @@
 
 #define TICK 100
 
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
 void initial_pos() {
     add_circle(100, 1, 100, 100, 0, 0);
     add_circle(5, 1, 300, 100, 0, 0);
@@ -116,6 +119,80 @@ int main()
                     if (strcmp(cmd, "v") == 0)
                     {
                         double old_abs = sqrt(circles[id]->vx * circles[id]->vx + circles[id]->vy * circles[id]->vy);
+                        circles[id]->vx /= old_abs;
+                        circles[id]->vy /= old_abs;
+                        circles[id]->vx *= val;
+                        circles[id]->vy *= val;
+                    }
+                }
+                if (strcmp(s, "max") == 0)
+                {
+                    int id;
+                    double val;
+                    fscanf(f_i, "%d %s %lf", &id, cmd, &val);
+
+                    #ifdef DEBUG
+                        printf("%d %lf\n", id, val);
+                    #endif
+
+                    if (id < 0 || id > circles_count)
+                    {
+                        continue;
+                    }
+
+                    if (strcmp(cmd, "x") == 0)
+                        circles[id]->x = min(val, circles[id]->x);
+                    if (strcmp(cmd, "y") == 0)
+                        circles[id]->y = min(val, circles[id]->y);
+                    if (strcmp(cmd, "r") == 0)
+                        circles[id]->r = min(val, circles[id]->r);
+                    if (strcmp(cmd, "m") == 0)
+                        circles[id]->m = min(val, circles[id]->m);
+                    if (strcmp(cmd, "vx") == 0)
+                        circles[id]->vx = min(val, circles[id]->vx);
+                    if (strcmp(cmd, "vy") == 0)
+                        circles[id]->vy = min(val, circles[id]->vy);
+                    if (strcmp(cmd, "v") == 0)
+                    {
+                        double old_abs = sqrt(circles[id]->vx * circles[id]->vx + circles[id]->vy * circles[id]->vy);
+                        val = min(val, old_abs);
+                        circles[id]->vx /= old_abs;
+                        circles[id]->vy /= old_abs;
+                        circles[id]->vx *= val;
+                        circles[id]->vy *= val;
+                    }
+                }
+                if (strcmp(s, "min") == 0)
+                {
+                    int id;
+                    double val;
+                    fscanf(f_i, "%d %s %lf", &id, cmd, &val);
+
+                    #ifdef DEBUG
+                        printf("%d %lf\n", id, val);
+                    #endif
+
+                    if (id < 0 || id > circles_count)
+                    {
+                        continue;
+                    }
+
+                    if (strcmp(cmd, "x") == 0)
+                        circles[id]->x = max(val, circles[id]->x);
+                    if (strcmp(cmd, "y") == 0)
+                        circles[id]->y = max(val, circles[id]->y);
+                    if (strcmp(cmd, "r") == 0)
+                        circles[id]->r = max(val, circles[id]->r);
+                    if (strcmp(cmd, "m") == 0)
+                        circles[id]->m = max(val, circles[id]->m);
+                    if (strcmp(cmd, "vx") == 0)
+                        circles[id]->vx = max(val, circles[id]->vx);
+                    if (strcmp(cmd, "vy") == 0)
+                        circles[id]->vy = max(val, circles[id]->vy);
+                    if (strcmp(cmd, "v") == 0)
+                    {
+                        double old_abs = sqrt(circles[id]->vx * circles[id]->vx + circles[id]->vy * circles[id]->vy);
+                        val = max(val, old_abs);
                         circles[id]->vx /= old_abs;
                         circles[id]->vy /= old_abs;
                         circles[id]->vx *= val;
